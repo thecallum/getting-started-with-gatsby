fdf2fd5c9f21f92550920cbfff4c2ec3c20f2337
Callum Macpherson <callummac@protonmail.com>
2019-11-30 13:46:01 +0000

    Setup first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,69 @@
 import React from "react"
+import "../css/main.css"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <>
+    <nav class="navbar">
+      <div className="container">
+        <div className="navbar-container">
+          <h1 className="navbar-title">
+            <a href="/">Navbar</a>
+          </h1>
+        </div>
+      </div>
+    </nav>
+
+    <main className="main">
+      <div className="content">
+        <div className="container">
+          <h1>Hello world</h1>
+          <br />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+            praesentium dolore quidem laudantium quae! Consequuntur ducimus
+            facilis cumque nemo eligendi! Quibusdam itaque officia repudiandae
+            qui laboriosam, rerum minus tempora voluptates.
+          </p>
+        </div>
+      </div>
+
+      <div className="backdrop">
+        <div className="backdrop-content">
+          <div className="content">
+            <div className="container">
+              <h2>Lorem Ipsum</h2>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                unde quasi a dolorem hic labore molestias sapiente? Ducimus,
+                mollitia necessitatibus. Reiciendis delectus dignissimos animi
+                voluptas ratione, adipisci beatae incidunt molestias.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="content">
+        <div className="container">
+          <h2>Lorem Ipsum</h2>
+          <br />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+            praesentium dolore quidem laudantium quae! Consequuntur ducimus
+            facilis cumque nemo eligendi! Quibusdam itaque officia repudiandae
+            qui laboriosam, rerum minus tempora voluptates.
+          </p>
+        </div>
+      </div>
+    </main>
+
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-container">
+          <h3 class="footer-title">Footer</h3>
+        </div>
+      </div>
+    </footer>
+  </>
+)
